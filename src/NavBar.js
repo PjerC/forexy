0aff77338d74e302ebdaa6c690c8b043f2a726ce
PjerC <piotrcieadv2@gmail.com>
2021-12-07 19:55:23 +0100

    ok

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,7 +2,13 @@ import  './NavBarStyle.css'
 
 const NavBar = (props) => {
     return(
-            <div className="navBarStyle"> Navigation bar </div>
+            <div className="navBarStyle">
+                <div style={{display:"flex",flexDirection:"row"}} >
+                    <a  className="linkBtn" target=""> btn1 </a>
+                    <a  className="linkBtn" target=""> btn1 </a>
+                    <a  className="linkBtn" target=""> btn1 </a>
+                </div>
+            </div>
     )
 }
 
