adf121593ea2852564bcabde12ec9c62138f73d7
PjerC <piotrcieadv2@gmail.com>
2021-12-07 22:06:01 +0100

    navbar added /w btns bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 //import logo from './logo.svg';
 import './App.css';
+import NavBar from './NavBar.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"> header
-      </header>
+      <NavBar/>
     </div>
   );
 }
---
+++ src/NavBar.js
@@ -0,0 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import logo from './img/navBar/logo_RP.PNG' 
+const NavBar = (props) => {
+
+
+return(
+
+    <div>
+        <nav className="navbar navbar-dark bg-dark">
+            <div className="container-fluid justify-content-evenly">
+                <button className="flex-fill btn btn-lg btn-outline-secondary m-2" type="button"> Strategie </button>
+                <button className="flex-fill btn btn-lg btn-outline-secondary m-2" type="button"> Materiały </button>
+                <button className="flex-fill btn btn-lg btn-outline-secondary m-2" type="button"> Testy </button>
+                <button className="flex-fill btn btn-lg btn-outline-secondary m-2" type="button"> Zagrania </button>
+            </div>
+        </nav>
+    </div>
+       
+    )
+}
+
+export default NavBar;
