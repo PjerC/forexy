4e7db0760892a9177382c14cd4700185b5a94fdd
PjerC <piotrcieadv2@gmail.com>
2021-12-08 21:35:08 +0100

    added bootstrap icons to nav bar btn

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-// import logo from './img/navBar/logo_RP.PNG' 
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 const NavBar = (props) => {
 
 
@@ -9,10 +10,16 @@ return(
         <nav className="navbar navbar-dark border border-secondary"> 
             <div className="container-fluid justify-content-evenly">
                 <button className="flex-fill btn btn-lg btn-outline-secondary m-1" type="button">
-                    <i class="bi bi-bar-chart"></i> Strategie img</button>
-                <button className="flex-fill btn btn-lg btn-outline-secondary m-1" type="button"> Materiały img</button>
-                <button className="flex-fill btn btn-lg btn-outline-secondary m-1" type="button"> Testy img</button>
-                <button className="flex-fill btn btn-lg btn-outline-secondary m-1" type="button"> Zagrania img</button>
+                    <i class="bi bi-file-earmark-bar-graph"> Strategie </i></button>
+                <button className="flex-fill btn btn-lg btn-outline-secondary m-1" type="button">
+                    <i class="bi bi-journal-bookmark-fill"> Materiały </i>
+                </button>
+                <button className="flex-fill btn btn-lg btn-outline-secondary m-1" type="button">
+                    <i class="bi bi-file-earmark-text"> Testy </i>
+                </button>
+                <button className="flex-fill btn btn-lg btn-outline-secondary m-1" type="button">
+                    <i class="bi bi-bar-chart"> Zagrania</i>
+                </button>
             </div>
         </nav>
     </div>
