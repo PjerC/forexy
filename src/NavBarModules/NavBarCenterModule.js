bb6c69891bd18b5f2458e6a1bcc21fe4154b053d
PjerC <piotrcieadv2@gmail.com>
2021-12-11 18:26:00 +0100

    ok

--- src/NavBarModules/NavBarCenterModule.js
+++ src/NavBarModules/NavBarCenterModule.js
@@ -3,39 +3,45 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './NavBarStyles.css';
 
+    // TO DO przerobic stany widocznosci na jeden obiekt
 const NavBarCenterModule = (props) => {
-    const [btnBackgroundColor,setBtnBackgroundColor] = useState('text-primary')
+    const [btnBackgroundColor,setBtnBackgroundColor] = useState('none')
     const {visibilityStrategy,setVisibilityStrategy,visibilityMaterial,setVisibilityMaterial,visibilityTests,setVisibilityTests,visibilityPositions,setVisibilityPositions} = props;
     // visibility functions management 
-    const hideDisplayModules = () => {
-        // przerobic na obiekt 
+    const hideOthenThanStrategyModules = () => {
         setVisibilityTests(false);
-        setVisibilityStrategy(false);
         setVisibilityPositions(false);
         setVisibilityMaterial(false);
-    }
-    //TODO  onclick przekierowuje na nastepna strone z contentem - ze strony podgladu 
+    } 
 
     const showStrategyModule = () => {
-        setVisibilityStrategy(!visibilityStrategy);
+        // btn
+        setBtnBackgroundColor('underline')
+        //modules
+        setVisibilityStrategy(true);
         setVisibilityMaterial(false);
         setVisibilityTests(false);
         setVisibilityPositions(false);
     };
     const showMaterialModule = () => {
+        // btn underline visibility
+        setBtnBackgroundColor('none')
         setVisibilityMaterial(!visibilityMaterial);
         setVisibilityStrategy(false);
         setVisibilityTests(false);
         setVisibilityPositions(false);
     };
     const showTestsModule = () => {
+        // btn underline visibility
+        setBtnBackgroundColor('none')
         setVisibilityTests(!visibilityTests);
         setVisibilityMaterial(false);
         setVisibilityStrategy(false);
         setVisibilityPositions(false);
     }
     const showPositionsModule = () => {
-
+        // btn underline visibility
+        setBtnBackgroundColor('none')
         setVisibilityPositions(!visibilityPositions);
         setVisibilityMaterial(false);
         setVisibilityStrategy(false);
@@ -44,13 +50,13 @@ const NavBarCenterModule = (props) => {
     return(
     
         <div>           
-            <button className="flex-fill btn btn-lg text-secondary m-1 navBarBtn shadow-none " type="button" onMouseEnter={showStrategyModule} >
+            <button  style={{textDecoration:btnBackgroundColor}} className="flex-fill btn btn-lg text-secondary btnBackgroundColor m-1 navBarBtn shadow-none " type="button" onMouseEnter={showStrategyModule} onMouseLeave={hideOthenThanStrategyModules}>
                 <i class="bi bi-file-earmark-bar-graph navBarBtn shadow-none"></i> Strategia  </button>
-            <button className="flex-fill btn btn-lg text-secondary m-1 navBarBtn shadow-none" type="button" onMouseEnter={showMaterialModule} onMouseLeave={hideDisplayModules}>
+            <button className="flex-fill btn btn-lg text-secondary m-1 navBarBtn shadow-none" type="button" onMouseEnter={showMaterialModule} >
                 <i class="bi bi-journal-bookmark-fill "> </i> Materiały </button>
-            <button className="flex-fill btn btn-lg text-secondary m-1 navBarBtn shadow-none" type="button" onMouseEnter={showTestsModule} onMouseLeave={hideDisplayModules} >
+            <button className="flex-fill btn btn-lg text-secondary m-1 navBarBtn shadow-none" type="button" onMouseEnter={showTestsModule} >
                 <i class="bi bi-file-earmark-text"></i> Testy </button>
-            <button className="flex-fill btn btn-lg text-secondary m-1 navBarBtn shadow-none" type="button" onMouseEnter={showPositionsModule} onMouseLeave={hideDisplayModules} >
+            <button className="flex-fill btn btn-lg text-secondary m-1 navBarBtn shadow-none" type="button" onMouseEnter={showPositionsModule}  >
                 <i class="bi bi-bar-chart"></i> Zagrania </button>
         </div>
 
