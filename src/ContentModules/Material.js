66cc7376c2a0d6dcd29873cb3885be206bcce03f
PjerC <piotrcieadv2@gmail.com>
2021-12-11 16:35:51 +0100

    content preview for strategy added

--- src/ContentModules/Material.js
+++ src/ContentModules/Material.js
@@ -3,6 +3,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const Material = (props) => {
 
+    const objMaterials = [1,2,3,4,5,6,7,8]
     /* grid 4 x 4  materialy od 1 do 8
         ikony + temat
         jesli hover to zwiekszenie i pokazanie contentu
--- src/ContentModules/Stategy.js
+++ src/ContentModules/Stategy.js
@@ -3,8 +3,8 @@ import './Strategy.css';
 const Strategy = (props) => {
 
    const objStrategies = [
-        {strategyName:'fibo',previewImgUrl: 'img1'},
-        {strategyName:'trendline',previewImgUrl: 'img2'},
+        {strategyName:'fibonacci',previewImgUrl: 'https://drive.google.com/uc?id=10j7L5EJ_4Z7_IZXY3XqtRxn0Gxz6GUCk' },
+        {strategyName:'trendline',previewImgUrl: 'https://drive.google.com/uc?id=1cLPpUEJtRg5y_AhB0iYOFRUH4vv032bA'},
         {strategyName:'divergence',previewImgUrl: 'img3'},
         {strategyName:'support resistance',previewImgUrl: 'img4'},
         {strategyName:'price action',previewImgUrl: 'img5'},
@@ -13,12 +13,18 @@ const Strategy = (props) => {
     
     ];
    //TO DO - zakodzic metode do animowania kazdego elementu objektu - OSOBNO  , delay iteration ?
+   //TO DO - kolor kontentu zmiana na czarny z useState App.js
+   //TO DO - spinner - img loading 
 
     return (
 
-        <div className="d-flex h-100 justify-content-center align-items-center flex-grow-1 m-2 "> {
+        <div style={{animation: 'fadeIn 1s'}}  className="d-flex  text-primary h-100 justify-content-center align-items-center flex-grow-1 m-2 "> {
             objStrategies.map((item) => 
-                <div style={{animation: 'fadeIn 1s'}} className="d-flex justify-content-center w-50 h-50 g-row-6 g-col-md-4 p-2 border border-secondary rounded m-2 " key={item.strategyName}><p style={{animation: 'fadeIn 1s'}}>{item.strategyName}</p>
+                <div 
+                    className="d-flex flex-column justify-content-center align-items-center w-50 h-50 g-row-6 g-col-md-4 p-2 border border-secondary rounded m-2 " 
+                    key={item.strategyName}>
+                        <p style={{animation: 'fadeIn 1s'}} className='fw-bold font-monospace'>{item.strategyName}</p>
+                        <img className='img-fluid m-1 p-1 fadeIn 2s border border-secondary rounded' src={item.previewImgUrl} alt='none'/>
                  </div>)}
         </div>
     )
