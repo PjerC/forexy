baf87fd01bd99e60443dafef38a1d29f2309e9c1
PjerC <piotrcieadv2@gmail.com>
2021-12-14 18:46:22 +0100

    responsive work

--- src/ContentModules/Material.js
+++ src/ContentModules/Material.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './ContentModules.css';
-import { useState } from 'react';
+//import { useState } from 'react';
 
 
 const Material = (props) => {
