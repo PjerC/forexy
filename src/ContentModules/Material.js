0d610a044ec87c1e4fc51e0daf89b2c593e03051
PjerC <piotrcieadv2@gmail.com>
2021-12-14 18:44:29 +0100

    responsive work

--- src/ContentModules/Material.js
+++ src/ContentModules/Material.js
@@ -6,22 +6,20 @@ import { useState } from 'react';
 
 const Material = (props) => {
     //states
-    const [icon,setIcon] = useState('bi bi-folder')
+   // const [icon,setIcon] = useState('bi bi-folder')
     // pl
     const objMaterials = [
         {no:'1.', title: 'Podstawy'},
-        {no:'2.', title: 'Aplikacje', icon:{icon}},
-        {no:'3.', title: 'Strategie', icon:{icon}},
-        {no:'4.', title: 'Makro', icon:{icon}},
-        {no:'5.', title: 'Ryzyko', icon:{icon}},
-        {no:'6.', title: 'Notatnik', icon:{icon}},
-        {no:'7.', title: 'Słownik', icon:{icon}},
-        {no:'8.', title: 'Mentoring', icon:{icon}},
-        {no:'9.', title: 'Pozostałe', icon:{icon}}
+        {no:'2.', title: 'Aplikacje'},
+        {no:'3.', title: 'Strategie'},
+        {no:'4.', title: 'Makro'},
+        {no:'5.', title: 'Ryzyko'},
+        {no:'6.', title: 'Notatnik'},
+        {no:'7.', title: 'Słownik'},
+        {no:'8.', title: 'Mentoring'},
+        {no:'9.', title: 'Pozostałe'}
     ]
-    const changeIcon = () => {
-        setIcon('bi bi-folder2-open')
-    }
+
     // TO DO - zmienim onmouseleave / enter na jedna funkcje   typy false/true 
     //const changeFolderImg = () => {setFolderImg('text-secondary bi bi-folder2-open')}
     //const changeImgToInitial = () => {setFolderImg('text-secondary bi bi-folder')}
@@ -30,9 +28,9 @@ const Material = (props) => {
             <div className='row justify-content-md-center align-items-center m-1'>
                 {
                     objMaterials.map((item) => 
-                        <div className='col w-25 border border-secondary m-2 animScale rounded'>
-                            <i className={icon}></i>
-                            <p className='fs-6'>{item.no}{item.title}</p>
+                        <div className='col w-25 border border-secondary m-2 animScale rounded' key={item.no}>
+                            <i className='bi bi-folder'></i>
+                            <p className='fs-6'>{item.no+item.title}</p>
                         </div>                                
                 )}
             </div>                        
