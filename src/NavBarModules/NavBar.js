1a285b16f337c7a5ab8827ee19cbf0db94ec282a
PjerC <piotrcieadv2@gmail.com>
2021-12-21 01:06:39 +0100

    offcanvass sass cutomisatin

--- src/NavBarModules/NavBar.js
+++ src/NavBarModules/NavBar.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import "bootstrap-icons/font/bootstrap-icons.css";
 import NavBarCenterModule from './NavBarCenterModule';
 import NavBarRightModule from './NavBarRightModule';
-
+import '../custom.scss';
 const NavBar = (props) => {
 
 const {visibilityStrategy,setVisibilityStrategy,visibilityMaterial,setVisibilityMaterial, visibilityTests,setVisibilityTests,visibilityPositions,setVisibilityPositions} = props;
@@ -13,9 +13,9 @@ return(
                 <a href="#offcanvaser" className="btn btn-lg navBarBtn text-secondary shadow-none border border-secondary m-1" type="button" data-bs-toggle="offcanvas" aria-expanded="false" aria-controls="collapser">
                     <i className="bi bi-list" ></i></a>
                     
-                <div id="offcanvaser" className='offcanvas offcanvas-start bg-dark' tabIndex="-1" aria-labelledby="offcanvasExampleLabel">
+                <div id="offcanvaser" className='d-flex p-1 border border-secondary  horizontal-width-10 offcanvas offcanvas-start bg-dark' tabIndex="-1" aria-labelledby="offcanvas1">
                 <div className="offcanvas-header">
-                    <h5 className='offcanvas-title text-secondary'><i className="bi bi-cash-coin"></i> Robie Piniondz </h5>
+                    <h5 className='offcanvas-title text-secondary fw-bold'><i className="bi bi-cash-coin"></i> Robie Piniondz </h5>
                 </div>
 
                 <div id='navBarContainer' className='d-flex flex-column justify-content-center m-1'>  
