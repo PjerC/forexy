ae806824dc3b3ff7cbd3792cc45c8ef6d28a4760
PjerC <piotrcieadv2@gmail.com>
2021-12-22 00:23:09 +0100

    strategy module preview design

--- src/ContentModules/StrategyItems.js
+++ src/ContentModules/StrategyItems.js
@@ -1,5 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "bootstrap-icons/font/bootstrap-icons.css";
+import imgFib from '../Img/strategy/fibo.png'
+import imgTrendline from '../Img/strategy/trendline.png'
+import imgDiv from '../Img/strategy/divergence.png'
+import imgSr from '../Img/strategy/sr.png'
+import imgPriceaction from '../Img/strategy/priceaction.png'
+import imgEmaSma from '../Img/strategy/emaSma.png'
+import imgPatterns from '../Img/strategy/patterns.png'
 
 const Strategy = (props) => {
     // nested components visiblity setters
@@ -10,13 +17,13 @@ const Strategy = (props) => {
     const {setVisibilityStrategyItems,setVisibilityStrategyPreview} = props;
     // object - future from database inputs
     const objStrategies = [
-        {strategyName:'I.', description: 'Zniesienie Fibonacci ',previewImgUrl: 'https://drive.google.com/uc?id=10j7L5EJ_4Z7_IZXY3XqtRxn0Gxz6GUCk', preview:'Wyznaczanie poziomów zniesienia 0.618 & 0.5. Określanie początku i końca ruchu oraz golden zone.' },
-        {strategyName:'II.', description: 'Linie Trendu',previewImgUrl: 'https://drive.google.com/uc?id=1cLPpUEJtRg5y_AhB0iYOFRUH4vv032bA', preview:'Rysowanie linii trendu w miejscu reakcji ceny. Określanie przebicia linii i testu poziomu jako potwierdzenie.'},
-        {strategyName:'III.', description: 'Dywergencje rsi',previewImgUrl: 'https://drive.google.com/uc?id=1bj2OtZ0ORoBvoXBFVCv8fXwIcaMstEUU', preview:'Znajdywanie prawidłowych dywergencji na podstawie wskaźnika rsi, w strefach wykupienia lub wyprzedania.'},
-        {strategyName:'IV.', description: 'Wsparcia i Opory',previewImgUrl: 'https://drive.google.com/uc?id=1lY-GpUSk7EJhD5TPHb31pM8AKr19ltAy',preview:'Wyznaczanie możliwych historycznych i przyszłych wsparć i oporów dla ceny. Teoria poziomów psychologicznych.'},
-        {strategyName:'V.', description: 'Struktury Price Action',previewImgUrl: 'https://drive.google.com/uc?id=14FC-sakmyIgJy0JXBjwzB9_IkkqcO5GO',preview:'Określanie struktury spadkowanej, wzrostowej lub konsolidacji. Wyznaczanie dołków i szczytów. BoS.'},
-        {strategyName:'VI.', description: 'Linie Ema i Sma',previewImgUrl: 'https://drive.google.com/uc?id=11n_JROa1Ajahxp8GmJpxQQ9gfKYbsvO1',preview:'Użycie linii ema, sma do określania aktualnego trendu, jego zmiany, oporu lub dodatkowych potwierdzeń.'},
-        {strategyName:'VII.', description: 'Schematy',previewImgUrl: 'https://drive.google.com/uc?id=1oRaVsDJdXH-z7Vccsg0tB6JdF5X4eRE9',preview:'Odnajdywanie powtarzających się historycznie schematów takich jak h&s, wedges, triple tops, triple bottoms,  waves.'},
+        {strategyName:'I.', description: 'Zniesienie Fibonacci ',previewImgUrl: imgFib, preview:'Wyznaczanie poziomów zniesienia 0.618 & 0.5. Określanie początku i końca ruchu oraz golden zone.' },
+        {strategyName:'II.', description: 'Linie Trendu',previewImgUrl: imgTrendline, preview:'Rysowanie linii trendu w miejscu reakcji ceny. Określanie przebicia linii i testu poziomu jako potwierdzenie.'},
+        {strategyName:'III.', description: 'Dywergencje rsi',previewImgUrl: imgDiv, preview:'Znajdywanie prawidłowych dywergencji na podstawie wskaźnika rsi, w strefach wykupienia lub wyprzedania.'},
+        {strategyName:'IV.', description: 'Wsparcia i Opory',previewImgUrl: imgSr,preview:'Wyznaczanie możliwych historycznych i przyszłych wsparć i oporów dla ceny. Teoria poziomów psychologicznych.'},
+        {strategyName:'V.', description: 'Struktury Price Action',previewImgUrl: imgPriceaction ,preview:'Określanie struktury spadkowanej, wzrostowej lub konsolidacji. Wyznaczanie dołków i szczytów. BoS.'},
+        {strategyName:'VI.', description: 'Linie Ema i Sma',previewImgUrl: imgEmaSma,preview:'Użycie linii ema, sma do określania aktualnego trendu, jego zmiany, oporu lub dodatkowych potwierdzeń.'},
+        {strategyName:'VII.', description: 'Schematy',previewImgUrl: imgPatterns,preview:'Odnajdywanie powtarzających się historycznie schematów takich jak h&s, wedges, triple tops, triple bottoms,  waves.'},
         {strategyName:'VIII.', description: 'Mix',previewImgUrl: '',preview:'Wykorzystanie wiedzy ze wszystkich strategii do znajdywania potwierdzeń do otwarcia pozycji.'}
     ];
 
@@ -51,7 +58,7 @@ const Strategy = (props) => {
                                     }
                                 }><i className='fs-2 bi bi-play'></i></button>
                                 </div>
-                                <img src={item.previewImgUrl} className='img-fluid m-3 p-3 rounded border border-secondary'/>
+                                <img src={item.previewImgUrl} className='img-fluid m-3 p-3 rounded border border-secondary' alt='none' />
                 </div>)
             }  
             </div>
